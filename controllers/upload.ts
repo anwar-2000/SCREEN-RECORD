3567a3c6111ab644f2a42a77374f9e837848afb0
anwar-2000 <khaledkhaouani5@gmail.com>
2023-07-07 16:36:27 +0100

    building front end

--- controllers/upload.ts
+++ controllers/upload.ts
@@ -1,18 +1,31 @@
 import { Request, Response } from 'express';
-import cloudinary from '../configs/cloudinary_config';
+import {UploadApiResponse} from "cloudinary"
+import cloudinary  from '../configs/cloudinary_config';
+import streamifier from 'streamifier';
 
 interface UploadRequest extends Request {
   body: {
-    videoPath: string;
+    videoData: string;
   };
 }
 
 export const upload = async (req: UploadRequest, res: Response): Promise<any> => {
   try {
-    const { videoPath } = req.body;
-    const result = await cloudinary.v2.uploader.upload(videoPath, {
-      resource_type: 'video',
-    });
+    const { videoData } = req.body;
+    const buffer = Buffer.from(videoData, 'base64');
+    const result = (await new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.v2.uploader.upload_stream(
+        { resource_type: 'video' },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else if (result) {
+            resolve(result);
+          }
+        }
+      );
+      streamifier.createReadStream(buffer).pipe(uploadStream);
+    })) as UploadApiResponse;
     res.json({ url: result.secure_url });
   } catch (error) {
     console.log('UPLOADING VIDEO ERROR :', error);
