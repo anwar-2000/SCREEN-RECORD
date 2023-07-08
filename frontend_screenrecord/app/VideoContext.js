e4e95133512c9a841d17ed9acf6b6938473d9bcc
anwar-2000 <khaledkhaouani5@gmail.com>
2023-07-08 16:22:55 +0100

    can't create the blob neither the URL

--- frontend_screenrecord/app/VideoContext.js
+++ frontend_screenrecord/app/VideoContext.js
@@ -2,20 +2,25 @@ import React, { createContext, useRef, useState, useEffect } from "react";
 
 const VideoContext = createContext({
   stream: null,
+  showVideo : false,
+  videoUrl : null,
   recordedVideo: { current: null },
   startRecording: () => {},
   StopCapture: () => {},
 });
 
-let url = "http://localhost:8000/upload";
+//let url = "http://localhost:8000/upload";
+let recordedChunks = [];
+const options = { mimeType: 'video/webm' };
+
 
 const ContextProvider = ({ children }) => {
+  const [showVideo, setShowVideo] = useState(true);
   const [stream, setStream] = useState(null);
+  const [videoUrl, setVideoUrl] = useState(null);
   const recordedVideo = useRef();
+  let mediaRecorder;
 
-  useEffect(() => {
-    console.log("STREAM" + " " +stream);
-   }, [stream]);
 
 
   let options = {
@@ -31,50 +36,81 @@ const ContextProvider = ({ children }) => {
     systemAudio: "exclude",
   };
 
-
+  useEffect(() => {
+    if (recordedVideo.current && stream) {
+      recordedVideo.current.srcObject = stream;
+      recordedVideo.current.play();
+    }
+  }, [stream]);
 
   const startRecording = async () => {
-    console.log("STARTING SCREEN CAPTURE" + " " + stream);
+    //console.log("STARTING SCREEN CAPTURE" + " " + stream);
     let captureStream;
     try {
       captureStream = await navigator.mediaDevices.getDisplayMedia(options);
     } catch (error) {
       console.log(error);
     }
-    console.log("WHILE CAPTURE" + " " + captureStream);
-    if (captureStream) {
+    //console.log("WHILE CAPTURE" + " " + captureStream);
       setStream(captureStream);
       if (recordedVideo.current) {
-        recordedVideo.current.srcObject = stream;
-        console.log("SRCoBJECT" + " " +recordedVideo.current.srcObject);
-      }
+        recordedVideo.current.srcObject = captureStream;
+       // console.log("SRCoBJECT" + " " + recordedVideo.current.srcObject);
     }
+     // Set up the MediaRecorder
+     mediaRecorder = new MediaRecorder(captureStream, options);
+     mediaRecorder.ondataavailable = (event) => {
+      console.log('ondataavailable', event.data.size);
+       if (event.data.size > 0) {
+         recordedChunks.push(event.data);
+         if (recordedChunks.length === 0) {
+          console.log('The recordedChunks array is empty');
+        } else {
+          console.log('The recordedChunks array is not empty');
+        }
+       }
+     };
+     mediaRecorder.start();
   };
-  useEffect(() => {
-    if (recordedVideo.current && stream) {
-      recordedVideo.current.srcObject = stream;
-      console.log("SRCoBJECT" + " " +recordedVideo.current.srcObject);
-      recordedVideo.current.play();
-    }},[])
+
+
 
   const StopCapture = async () => {
-    console.log("STOPPING CAPTURE" + " " + stream);
+    //console.log("STOPPING CAPTURE" + " " + stream);
     if (stream) {
       stream.getTracks().forEach((track) => {
         track.stop();
       });
+      // Stop the MediaRecorder
+      
+    
+      // Creating a Blob from the recorded data
+      console.log('recordedChunks 2', recordedChunks );
+      const blob = new Blob(recordedChunks, { type: 'video/webm' });
+      console.log('Blob size:', blob.size);
+      //console.log('Blob type:', blob.type);
+    // Creating a temporary URL for the Blob
+      setVideoUrl(URL.createObjectURL(blob));
 
-      const videoBlob = await fetch(recordedVideo.current?.src || "").then(
-        (response) => response.blob()
-      );
+    setShowVideo(false);
+    //console.log("URL 1 ", videoUrl )
 
-      const formData = new FormData();
-      formData.append("file", videoBlob);
-
+    if(mediaRecorder){
+      mediaRecorder.stop();      
+    }
+      
+    // Convert the Blob to a base64-encoded string
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    let base64data ;
+    reader.onloadend = async () => {
+    base64data = reader.result;
+  }
+    /*
       try {
         const response = await fetch(url, {
           method: "POST",
-          body: formData,
+          body: JSON.stringify({ videoData: base64data }),
         });
 
         if (response.ok) {
@@ -90,7 +126,7 @@ const ContextProvider = ({ children }) => {
         }
       } catch (error) {
         console.error("Error uploading video to Cloudinary:", error);
-      }
+      }*/
     }
   };
 
@@ -100,6 +136,8 @@ const ContextProvider = ({ children }) => {
         stream,
         startRecording,
         StopCapture,
+        showVideo,
+        videoUrl,
         recordedVideo,
       }}>
       {children}
@@ -107,4 +145,4 @@ const ContextProvider = ({ children }) => {
   );
 };
 
-export { ContextProvider, VideoContext };
+export { ContextProvider, VideoContext }
--- frontend_screenrecord/app/components/Video.tsx
+++ frontend_screenrecord/app/components/Video.tsx
@@ -1,27 +1,30 @@
 "use client"
-
-import React, { useContext } from 'react'
+"use client"
+import React, { useContext, useEffect } from 'react'
 import styles from "./styles/video.module.css"
 import { VideoContext } from '../VideoContext'
 
 const Video = () =>{
         const {stream ,
+          showVideo,
+          videoUrl,
             recordedVideo,
             StopCapture,
         } = useContext(VideoContext);
 
         let disabled = !stream || !recordedVideo ;
-        console.log("stream and ref :" , stream , recordedVideo)
+        useEffect(()=>{
+
+        },[stream , recordedVideo])
   return (
     <>
         <div className={styles.container__video}>
-        <div className={styles.video}>{stream && <video ref={recordedVideo} autoPlay muted width={450} height={350}/>}</div> 
-        {!stream && <h1>loading %...</h1>}
-
+       { showVideo && <div className={styles.video}>{stream &&  <video  playsInline muted ref={recordedVideo} autoPlay  width={350}/>}</div>}
+       {!showVideo &&  <div className={styles.video}>{stream &&  <video  playsInline controls src={videoUrl as unknown as string}  width={350}/>}</div> }
         <div className={styles.actions}>
                 <button disabled={disabled} onClick={StopCapture}>STOP RECORDING</button>
                 <button>SAVE IN GALLERY</button>
-                <button disabled={disabled}>DOWNLOAD</button>
+                <a href={videoUrl as unknown as string} download>Download</a>
         </div>
         </div>
       
